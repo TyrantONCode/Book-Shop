2298c78a914654cf0df53450958297b7e3f89224
Reza <1984.rezasoleymani590@gmail.com>
2022-07-01 23:36:02 -0700

    userlogin window added

---
+++ Book Shop/UserLoginWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Book_Shop
+{
+    /// <summary>
+    /// Interaction logic for UserLogin.xaml
+    /// </summary>
+    public partial class UserLoginWindow : Window
+    {
+        public UserLoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            string email = this.EmailTextbox.Text;
+            string password= this.PasswordTextbox.Text;
+            //todo: data base code here :
+        }
+    }
+}
