088d5963054dec6e1cd10b1eb74f3520c9a16790
Ali Samadifard <ali.ali.samadifard@gmail.com>
2022-07-03 05:48:22 +0430

    Adding data and Logging in and Checking username uniqueness

--- Book Shop/MainWindow.xaml.cs
+++ Book Shop/MainWindow.xaml.cs
@@ -22,7 +22,16 @@ namespace Book_Shop
     {
         public MainWindow()
         {
-            
+
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            //UserDbManager.AddData(id_textbox.Text.ToString(), firstname_textbox.Text.ToString(),
+            //    lastname_textbox.Text.ToString(), password_textbox.Text.ToString(),
+            //    email_textbox.Text.ToString(), phone_textbox.Text.ToString(),
+            //    float.Parse(money_textbox.Text.ToString()));
+            textblockshit.Text = UserDbManager.ValidUsername(id_textbox.Text.ToString()).ToString();
         }
     }
 }
--- Book Shop/UserDbManager.cs
+++ Book Shop/UserDbManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Microsoft.Data.SqlClient;
+using System.IO;
 
 namespace Book_Shop
 {
@@ -16,20 +17,57 @@ namespace Book_Shop
         public string email;
         public string phone;
         public float money;
+        public static string root;
+        public static string connectionKey;
         // public List<Book> bag;
         // public List<Book> bought;
         // public List<Book> bookmarks;
-        public void AddData(int id,string firstname, string lastname,
+        public static void AddData(string firstname, string lastname,string username,
             string password, string email, string phone, float money)
         {
-            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=&quotC: \Users\Armageddon\Desktop\Ap proj\Book - Shop\Book Shop\UserDb.mdf&quot; Integrated Security=True;Connect Timeout=30");
+            root = Path.GetFullPath("App_Data\\UserDataBase.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
+            connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(connectionKey);
             conn.Open();
-            string command = "insert into [Table] values ('"+id+"','"+firstname+"','"+lastname+"','"+password+"','"+email+"','"+phone+"','"+money+"')";
+            string command = "select max(id) + 1 from [Table]";
             SqlCommand cmd = new SqlCommand(command, conn);
+            int id = Convert.ToInt32(cmd.ExecuteScalar());
+            command = "insert into [Table] values ('" + id + "','" + firstname + "','" + lastname + "','" + username + "','" + password + "','" + email + "','" + phone + "','" + money + "')";
+            cmd = new SqlCommand(command, conn);
             cmd.ExecuteNonQuery();
             conn.Close();
         }
-        
+
+        public static bool UserLogin(string username, string password)
+        {
+            root = Path.GetFullPath("App_Data\\UserDataBase.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
+            connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(connectionKey);
+            conn.Open();
+            string command = "select * from [Table] where username like '"+username+"' and password like '"+password+"'";
+            SqlCommand cmd = new SqlCommand(command, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            bool output = reader.Read();
+            conn.Close();
+            return output;
+        }
+
+        public static bool ValidUsername(string username)
+        {
+            root = Path.GetFullPath("App_Data\\UserDataBase.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
+            connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(connectionKey);
+            conn.Open();
+            string command = "select * from [Table] where lower(username) like '"+username.ToLower()+"'";
+            SqlCommand cmd = new SqlCommand(command, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            bool output = reader.Read();
+            conn.Close();
+            return !output;
+        }
+
+
 
     }
+
 }
