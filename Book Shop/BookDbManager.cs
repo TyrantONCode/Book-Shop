e2a9fdfa0bfa54e6c5e8bc44e3f9c08ef71a8991
Ali Samadifard <ali.ali.samadifard@gmail.com>
2022-07-07 20:28:19 +0430

    ADD: DbManager

--- Book Shop/BookDbManager.cs
+++ Book Shop/BookDbManager.cs
@@ -9,88 +9,68 @@ using System.IO;
 
 namespace Book_Shop
 {
-    //internal class BookDbManager
-    //{
-    //    public static string root;
-    //    public static string connectionKey;
+    internal class BookDbManager
+    {
+        public static string root;
+        public static string connectionKey;
+        public static string imageRoot;
 
-    //    public static void AddData(string title, string writer,
-    //        int publishYear, string brief, int phone, string money)
-    //    {
-    //        root = Path.GetFullPath("App_Data\\BookDb.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
-    //        connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
-    //        SqlConnection conn = new SqlConnection(connectionKey);
-    //        string books = "";
-    //        conn.Open();
-    //        string command = "select max(id) + 1 from [Table]";
-    //        SqlCommand cmd = new SqlCommand(command, conn);
-    //        int id = Convert.ToInt32(cmd.ExecuteScalar());
-    //        command = "insert into [Table] values ('" + id + "','" + title + "','" +  + "','" + password + "','" + email + "','" + phone + "','" + money + "','" + books + "')";
-    //        cmd = new SqlCommand(command, conn);
-    //        cmd.ExecuteNonQuery();
-    //        conn.Close();
-    //    }
+        public static void AddData(string title, string writer,
+            int publishYear, string brief, float cost, string imagepath)
+        {
+            root = Path.GetFullPath("App_Data\\BookDb.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
+            connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(connectionKey);
+            string books = "";
+            conn.Open();
+            string command = "select max(id) + 1 from [Table]";
+            SqlCommand cmd = new SqlCommand(command, conn);
+            int id = 0;
+            try
+            {
+                id = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (InvalidCastException)
+            {
+                id = 0;
+            }
+            command = "insert into [Table] values ('" + id + "','" + title + "','" + writer +"','" + publishYear + "','" + brief + "','" + cost + "','"+imagepath+"')";
+            cmd = new SqlCommand(command, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
 
-    //    public static bool UserLogin(string email, string password)
-    //    {
-    //        root = Path.GetFullPath("App_Data\\UserDataBase.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
-    //        connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
-    //        SqlConnection conn = new SqlConnection(connectionKey);
-    //        conn.Open();
-    //        string command = "select * from [Table] where email like '" + email + "' and password like '" + password + "'";
-    //        SqlCommand cmd = new SqlCommand(command, conn);
-    //        SqlDataReader reader = cmd.ExecuteReader();
-    //        bool output = reader.Read();
-    //        conn.Close();
-    //        return output;
-    //    }
-
-    //    public static bool ValidLoginEmail(string email)
-    //    {
-    //        root = Path.GetFullPath("App_Data\\UserDataBase.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
-    //        connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
-    //        SqlConnection conn = new SqlConnection(connectionKey);
-    //        conn.Open();
-    //        string command = "select * from [Table] where email like '" + email + "'";
-    //        SqlCommand cmd = new SqlCommand(command, conn);
-    //        SqlDataReader reader = cmd.ExecuteReader();
-    //        bool output = reader.Read();
-    //        conn.Close();
-    //        return output;
-    //    }
-
-
-    //    public static string AddFavBooks(string favbookstring, int book_id)
-    //    {
-    //        if (favbookstring.Length == 0)
-    //        {
-    //            favbookstring += book_id;
-    //        }
-    //        else
-    //        {
-    //            favbookstring += $" {book_id}";
-    //        }
-    //        return favbookstring;
-    //    }
-
-
-    //    public static int[] FavBookIdList(string email)
-    //    {
-    //        root = Path.GetFullPath("App_Data\\UserDataBase.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
-    //        connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
-    //        SqlConnection conn = new SqlConnection(connectionKey);
-    //        conn.Open();
-    //        string command = "select * from [Table] where email like '" + email + "'";
-    //        SqlCommand cmd = new SqlCommand(command, conn);
-    //        SqlDataReader reader = cmd.ExecuteReader();
-    //        string[] favbooks = reader.GetString(7).Split();
-    //        conn.Close();
-    //        int[] output = new int[favbooks.Length];
-    //        for (int i = 0; i < favbooks.Length; i++)
-    //        {
-    //            output[i] = int.Parse(favbooks[i]);
-    //        }
-    //        return output;
-    //    }
-    //}
+        public static bool ShowData(int id, out string title, out string writer,
+            out int publishyear,out string breif, out float cost, out string imagepath)
+        {
+            root = Path.GetFullPath("App_Data\\UserDataBase.mdf").ToString().Replace(@"Book Shop\bin\Debug\net6.0-windows\", "");
+            connectionKey = @$"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={root};Integrated Security=True;Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(connectionKey);
+            conn.Open();
+            string command = "select * from [Table] where id like '" + id + "'";
+            SqlCommand cmd = new SqlCommand(command, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            bool output = reader.Read();
+            conn.Close();
+            if (output)
+            {
+                title = reader.GetString(0);
+                writer = reader.GetString(1);
+                publishyear = reader.GetInt32(2);
+                breif = reader.GetString(3);
+                cost = reader.GetFloat(4);
+                imagepath = reader.GetString(5);
+            }
+            else
+            {
+                title = "";
+                writer = "";
+                publishyear = 0;
+                breif = "";
+                cost = 0;
+                imagepath = "";
+            }
+            return output;
+        }
+    }
 }
